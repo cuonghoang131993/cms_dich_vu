45948162fcb126e6a07b75cdbc06f7b34ef172ae
cuonghoang <cuonghoang131993@gmail.com>
2025-01-19 22:36:09 +0700

    add styles

--- src/api/product/services/product.ts
+++ src/api/product/services/product.ts
@@ -25,7 +25,6 @@ export default {
         ...options
       } as any);
     } catch (err) {
-      console.log(err)
       return err;
     }
   },
--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -1,6 +1,5 @@
 module.exports = {
   removeDuplicates(myArr, key) {
-    console.log(myArr, key)
     return [...new Map(myArr.map(item => [item[key], item])).values()]
   },
 };
